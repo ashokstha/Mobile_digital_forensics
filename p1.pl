9990ba15fd833cdda1aef51f0e17dce6607b8117
akstha007 <cs.ashok007@gmail.com>
2018-09-06 21:57:11 -0500

    parameter conditions added

--- p1.pl
+++ p1.pl
@@ -2,34 +2,41 @@
 use strict;
 use warnings;
 
-my $num_of_params = @ARGV;
-
-if($num_of_params>2){
+sub display_errors{
 	print "Error! Incorrect syntax!\n";
 	print("Use...\nperl filename.pl -(phonenumbers | emails) -[phonenumbers | emails]\n")
-}else{
-	if($num_of_params==1){
-		my $param0 = $ARGV[0];
-		my $expr;
-		if($param0 eq "-phonenumbers"){ 
-			print "Fetching phonenumbers ...\n";
-			$expr = "^[[0-9]{3}[-]?]{2}[0-9]{4}\$";
-			print "Reg expr: $expr\n"
-		}elsif($param0 eq "-emails"){
-			print "Fetching emails ...\n";
-			#$expr = "^[\w]+[\.\-]*[\w]*@[\w]+\.[\w]{1,3}\$";
-			print "Reg expr: \n"
-		}
-	}else{
-		if($num_of_params==2){
-			my $param0 = $ARGV[0];
-			my $param1 = $ARGV[1];
-			if(not (($param0 eq "-phonenumbers" and $param1 eq "-emails") or ($param1 eq "-phonenumbers" and $param0 eq "-emails"))){
-				print "Error! Incorrect syntax!\n";
-				print("Use...\nperl filename.pl -(phonenumbers | emails) -[phonenumbers | emails]\n")
-			}
-		}
-
-		print "Fetching phonenumbers and emails ...\n";
-	}
 }
+
+sub fetch_emails{
+	print "Fetching emails ...\n";
+	print "Reg expr: \n";
+}
+
+sub fetch_numbers{
+	print "Fetching phonenumbers ...\n";
+	my $expr = "^[[0-9]{3}[-]?]{2}[0-9]{4}\$";
+	print "Reg expr: $expr\n";
+}
+
+sub fetch_emails_numbers{
+	print "Fetching emails and phonenumbers ...\n";
+}
+
+my $num_of_params = @ARGV;
+print "----------------------------------------------------------------------------\n";
+if($num_of_params==0 ){
+	fetch_emails_numbers();
+}elsif($num_of_params==1 and $ARGV[0]eq "-emails"){
+	fetch_emails();
+}elsif($num_of_params==1 and $ARGV[0] eq "-phonenumbers"){
+	fetch_numbers();
+}elsif($num_of_params==2){
+	if(($ARGV[0] eq "-phonenumbers" and $ARGV[1] eq "-emails") or ($ARGV[1] eq "-phonenumbers" and $ARGV[0] eq "-emails")){
+		fetch_emails_numbers();
+	}else{
+		display_errors();
+	}
+}else{
+	display_errors();
+}
+print "----------------------------------------------------------------------------\n";
