0d2ebc5784d6784047eb1259a26a9c28b9003ab4
akstha007 <cs.ashok007@gmail.com>
2018-09-20 23:09:35 -0500

    carver.cc code converted to python

--- Project 2/source.py
+++ Project 2/source.py
@@ -1,23 +1,63 @@
 #!/bin/python3
-import re
+import os
+import shutil
 
 inFile = "../../sheep.jji"
 
-def main():
+magic_numbers = {'jji_start': b'\x00\x4a\x00\x4f\x00\x53\x00\x48',
+                 'jji_end': b'\x00\x4a\x00\x4f\x00\x4e\x00\x45\x00\x53'}
+max_read_size = 10
 
+outpath = "output_files"
+
+
+def create_dir(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.makedirs(path)
+
+def save_file(filename, bytes):
+    with open(filename,"w+b") as f:
+    	f.seek(0)
+    	f.write(bytes)
+
+def main():
     print("Reading file ...")
 
-    pattern = re.compile("J.?O.?S.?H.?(\s|[\s\S])*\x01\x01")
+    BUF_SIZE = 10
+    start_addr = 0
+    end_addr = 0
 
-    with open(inFile, encoding='latin1') as f:
-        for line in f:
-            match = pattern.search(line, re.MULTILINE) 
-            if match:
-                print(match)
+    create_dir(outpath)
 
+    with open(inFile, "rb") as f:
+        buf = f.read(BUF_SIZE)
+        while buf:
+            if buf.startswith(magic_numbers['jji_start']):
+                start_addr = f.tell()
+                print("Start_addr: {0}".format(start_addr))
+            elif buf.startswith(magic_numbers['jji_end']):
+                end_addr = f.tell()
+                print("End_addr: {0}".format(end_addr))
+
+                size = end_addr - start_addr + BUF_SIZE
+                result_path = outpath + "/result.txt"
+                with open(result_path, "a+b") as r:
+                    f.seek(-size,1)
+
+                    #read data of small size and save into file
+                    bytes = f.read(size)
+                    r.write(bytes)
+                    
+                    f.seek(0,1)
+
+            else:
+                f.seek(-max_read_size + 1, 1)
+
+            buf = f.read(BUF_SIZE)
 
     print("Complete.")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
